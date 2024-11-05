b27cac9bddd25566da9a60549fee8764d8c36df1
Hy <hyhuynh222@gmail.com>
2024-11-05 07:16:11 -0800

    Added Day2prep

--- InterviewPrep/src/main/java/org/example/Main.java
+++ InterviewPrep/src/main/java/org/example/Main.java
@@ -1,16 +1,31 @@
 package org.example;
 
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
-        Dog dog = new Dog("Goldy");
-        Cat cat = new Cat("Moewsker");
+//        Dog dog = new Dog("Goldy");
+//        Cat cat = new Cat("Moewsker");
+//
+//        Animal myDog = dog;
+//        Animal myCat = cat;
+//
+//        myDog.sound();
+//        myCat.sound();
+//        myCat.sound("MEOW MEOW");
+        System.out.println(method("Hello"));    }
 
-        Animal myDog = dog;
-        Animal myCat = cat;
+    private static String method(String inpt){
+        HashSet<Character> hset = new HashSet<>();
+        String res = "";
+        for (int i = 0; i < inpt.length(); i++){
+            if (!(hset.contains(inpt.charAt(i)))){
+                res = res + inpt.charAt(i);
+                hset.add(inpt.charAt(i));
+            }
 
-        myDog.sound();
-        myCat.sound();
-        myCat.sound("MEOW MEOW");
+        }
+        return res;
     }
 
 
